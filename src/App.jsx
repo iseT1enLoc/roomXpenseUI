a5b5130a0bb1f31d63ecd8eb3853b3f820098d83
iseT1enLoc <locnvt.it@gmail.com>
2025-04-23 21:06:57 +0700

    add test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/Home';
 import SuccessPage from './pages/SuccessLoginPage';
 import RoomExpenditureDetails from './pages/RoomExpenditureDetail';
+import TestingMessage from './components/Test';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/successpage" element={<SuccessPage />} />
+          <Route path="/test" element={<TestingMessage/>} />
           <Route path="/room-expense-details" element={<RoomExpenditureDetails />} />
         </Routes>
         {/* Optionally you can add a footer here */}
---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestingMessage = () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h1>Testing</h1>
+    </div>
+  );
+};
+
+export default TestingMessage;
